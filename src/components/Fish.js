a270d747482bb09feccb94a7f1b1595a336b03fc
Patrick Lehmann <ptrcklehmann@gmail.com>
2019-02-14 21:14:52 +0100

    created fish component

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
+import { formatPrice } from '../helpers'
 
 export class Fish extends Component {
   render() {
+    const {details} = this.props
+    const isAvailable = details.status === 'available'
+    const buttonText = isAvailable ? 'Add to Order' : 'Sold Out'
     return (
-      <li className="menu-fish">fish</li>
+        
+      <li className="menu-fish">
+          <img src={details.image} alt={details.name} />
+          <h3 className="fish-name">{details.name}
+          <span className="price">{formatPrice(details.price)}</span>
+          </h3>
+          <p>{details.desc}</p>
+          <button disabled={!isAvailable}>{buttonText}</button>
+          
+          
+      </li>
     )
   }
 }
